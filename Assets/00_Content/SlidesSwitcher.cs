3b8becd7a3db3ec77460ce100d66172b3976ac69
Walter Hulsebos <walterhulsebos@outlook.com>
2023-09-21 12:25:57 +0200

    Rest

--- Assets/00_Content/SlidesSwitcher.cs
+++ Assets/00_Content/SlidesSwitcher.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Playables;
@@ -10,41 +11,114 @@ public class SlidesSwitcher : MonoBehaviour
     [SerializeField] private TimelineAsset[]  slides;
     
     [SerializeField] private InputActionReference nextSlide;
-    [SerializeField] private InputActionAsset previousSlide;
+    [SerializeField] private InputActionReference previousSlide;
 
     private int _currentSlideIndex;
     
-    private TimelineAsset CurrentSlide => slides[_currentSlideIndex];
+    private TimelineAsset CurrentSlide => slides[Mathf.Clamp(_currentSlideIndex, min: 0, max: slides.Length - 1)];
 
     private void OnEnable()
     {
-        nextSlide.action.Enable();
-        previousSlide.Enable();
+        if(nextSlide != null)
+        {
+            if (nextSlide.action != null)
+            {
+                nextSlide.action.Enable();
+            }
+        }
+
+        if(previousSlide != null)
+        {
+            if (previousSlide.action != null)
+            {
+                previousSlide.action.Enable();
+            }
+        }
     }
     
     private void OnDisable()
     {
-        nextSlide.action.Disable();
-        previousSlide.Disable();
+        if(nextSlide != null)
+        {
+            if (nextSlide.action != null)
+            {
+                nextSlide.action.Disable();
+            }
+        }
+
+        if(previousSlide != null)
+        {
+            if (previousSlide.action != null)
+            {
+                previousSlide.action.Disable();
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        _currentSlideIndex = 0;
     }
 
     private void Start()
     {
-        nextSlide.action.performed += OnNextSlide;
-        //previousSlide.performed += OnPreviousSlide;
+        if (nextSlide != null)
+        {
+            if (nextSlide.action != null)
+            {
+                nextSlide.action.performed += OnNextSlide;
+            }
+        }
+
+        if (previousSlide != null)
+        {
+            if (previousSlide.action != null)
+            {
+                previousSlide.action.performed += OnPreviousSlide;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnNextSlide(default);
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            OnPreviousSlide(default);
+        }
     }
 
     private void OnPreviousSlide(InputAction.CallbackContext obj)
     {
         _currentSlideIndex--;
-        
-        director.playableAsset = CurrentSlide;
+
+        if (CurrentSlide != null)
+        {
+            // director.playableAsset = CurrentSlide;
+            // director.time = 0;
+            director.Play(CurrentSlide);
+        }
     }
 
     private void OnNextSlide(InputAction.CallbackContext obj)
     {
+        bool isLastSlide = _currentSlideIndex == slides.Length - 1;
+        
+        if (isLastSlide)
+        {
+            return;
+        }
+        
         _currentSlideIndex++;
         
-        director.playableAsset = CurrentSlide;
+        if (CurrentSlide != null)
+        {
+            // director.playableAsset = CurrentSlide;
+            // director.time = 0;
+            director.Play(CurrentSlide);
+        }
     }
 }
